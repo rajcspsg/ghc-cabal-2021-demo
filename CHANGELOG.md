421e074fef4ad277350ea866824095e6951263d2
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2024-12-31 18:45:46 -0800

    ghc-2021 add unit tests

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for ghc-cabal2021-demo
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ tests/BasicSumTest.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import BasicSum
+import qualified System.Exit as Exit
+import Test.HUnit
+
+test1 :: Test
+test1 = TestCase (assertEqual "should return 3" 3 (basicSum 1 2))
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1]
+
+main :: IO ()
+main = do
+  result <- runTestTT tests
+  if failures result > 0 then Exit.exitFailure else Exit.exitSuccess
